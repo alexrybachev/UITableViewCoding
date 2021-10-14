588055f64622560c54b76e1e3988e5a36a6fd880
Alex Rybachev <rybachev.as@gmail.com>
2021-10-14 22:01:31 +0400

    elementary setting AppDelegate and ViewControoler

--- UITableViewCoding/ViewController.swift
+++ UITableViewCoding/ViewController.swift
@@ -11,7 +11,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
+        title = "Настройки"
+        navigationController?.navigationBar.prefersLargeTitles = false
+        
+    
     }
 
 
